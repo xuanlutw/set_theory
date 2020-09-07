28977de74ba03634608b8e3ad4379414d4bdeaec
xuan <s986309@gmail.com>
2020-09-07 12:36:13 +0800

    Remove old files

--- axiom/ax_choice.v
+++
@@ -1,8 +0,0 @@
-Require Import axiom.axiom.
-Require Import relation.relation.
-Require Import relation.function.
-
-(* Axiom of Choice *)
-Axiom ax_choice: forall R, rel R -> 
-  exists H, function H /\ H ⊆ R /\ dom(H) = dom(R).
-(*----------------------------------------------------------------------------*)
--- axiom/ax_infinity.v
+++
@@ -1,6 +0,0 @@
-Require Import axiom.axiom.
-Require Import nat.inductive.
-
-(* Axiom of Infinity *)
-Axiom ax_infinity: exists A, inductive A.
-(*----------------------------------------------------------------------------*)
